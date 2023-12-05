f30df5da20ad0cf0dc01e1f3a7401dd9b8b5a1e8
Max Samsonenko <samsonenko.maxwork@gmail.com>
2023-12-05 17:42:48 +0200

    first save

---
+++ intermediate/src/js/refs.js
@@ -0,0 +1,11 @@
+export const refs = {
+	container: document.querySelector("#container"),
+	form: document.querySelector(".form"),
+	textarea: document.querySelector(".task-descr"),
+	modal: document.querySelector(".backdrop"),
+	count: document.getElementById("the-count"),
+	currnet: document.getElementById("current"),
+	newTasksLsit: document.getElementById("new-tasks"),
+	tasksInProgress: document.getElementById("in-progress"),
+	completedTasks: document.getElementById("completed"),
+};
